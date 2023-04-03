3a718f8f3d118f8a18c61d901f3a48980f66ca53
KK <102746679+1572161937@users.noreply.github.com>
2023-04-03 15:59:51 +0800

    Update mail.py

--- mail.py
+++ mail.py
@@ -6,7 +6,7 @@ from email.header import Header
 import config
 
 # 第三方 SMTP 服务
-mail_host = "mxhm.qiye.163.com"   # 设置服务器
+mail_host = "smtp.ruc.edu.cn"  # 设置服务器
 mail_user = config.vruc_mailbox   # 用户名
 mail_pass = config.vruc_mail_password  # 口令
 
